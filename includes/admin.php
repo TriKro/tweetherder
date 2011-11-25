38051ea6564d3746d57a158cd15b55ee2ac10178
Peter Backx <peter.backx@gmail.com>
2011-11-25 10:18:28 +0100

    settings screen complete

--- includes/admin.php
+++ includes/admin.php
@@ -15,14 +15,31 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 */
 
 function tweetherder_admin_menu () {
-	$options = get_option('tweetherder_options'); 
-	// $options['twitter_name']
-	// $options['custom_css']
-	
 	add_options_page('TweetHerder', 'TweetHerder', 'manage_options', 'tweetherder', 'tweetherder_options_page');
 }
 
 function tweetherder_admin_init () {
+	register_setting('tweetherder_options', 'tweetherder_options');
+	
+	add_settings_section('tweetherder_main', 'Settings', 'tweetherder_main_text', 'tweetherder');
+	add_settings_field('tweetherder_twitter_name', 'Twitter username',      'tweetherder_twitter_name_input', 'tweetherder', 'tweetherder_main');
+	add_settings_field('tweetherder_custom_css',   'Custom CSS (optional)', 'tweetherder_custom_css_input',   'tweetherder', 'tweetherder_main');
+}
+
+function tweetherder_main_text() {
+	echo '<p>Configure the TweetHerder plugin here.</p>';
+}
+
+function tweetherder_twitter_name_input() {
+	$options = get_option('tweetherder_options');
+	$value   = $options['twitter_name'];
+	echo "<input id='twitter_name' name='tweetherder_options[twitter_name]' type='text' value='$value' />";
+}
+
+function tweetherder_custom_css_input() {
+	$options = get_option('tweetherder_options');
+	$value   = $options['custom_css'];
+	echo "<input id='custom_css' name='tweetherder_options[custom_css]' type='text' value='$value' />";
 }
 
 function tweetherder_options_page() {
@@ -31,6 +48,11 @@ function tweetherder_options_page() {
 		<?php screen_icon(); ?>
 		<h2>TweetHerder</h2>
 		<form action="options.php" method="post">
+			<?php
+				settings_fields('tweetherder_options');
+				do_settings_sections('tweetherder');
+			?>
+			<input name="submit" type="submit" value="Save Changes"/>
 		</form>
 	</div>
 	<?php
